2950c321f9862dfdf8979991ec60aafecd9f5eed
SeharAnsari007 <seharansari007@gmail.com>
2024-07-08 01:11:49 -0700

    Adding Number-Guessing Game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer'
+console.log("\n\tWelcome to code with Sehar - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt ([
+    {
+        name : "userGuessedNumber",
+        type : "number",
+        message : "Guess a number(From 1 to 5)",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber){
+    console.log("Congratulations! You have won")
+}
+else {
+    console.log("Try again!")
+}
